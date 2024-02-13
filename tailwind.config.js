7817b80481f5ceddbb2874a7a0bcaf2bc49fc644
Ruslan Mahats <ruslanmahats13@gmail.com>
2024-02-13 13:40:18 +0200

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ['./index.html'],
 	theme: {
-		extend: {},
+		extend: {
+			gridTemplateRows: {
+				layout: 'auto 1fr auto',
+			},
+			fontFamily: {
+				main: 'Montserrat',
+			},
+		},
 	},
 	plugins: [],
 };
